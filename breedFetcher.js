a12c2010e90fcdc2160432d2c89257ef397e8469
SorrenJ <soalexjao@gmail.com>
2024-04-27 17:53:05 -0700

    implemented edge cases for errors and invalid breeds

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,40 @@
-const fs = require('fs');
 let args = process.argv.slice(2);
 const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
-    request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
     // converts the JSON string into an actual object
     const data = JSON.parse(body);
-console.log(data);
-console.log(typeof data);
+    console.log(data);
+    console.log(typeof data);
 
-// Access the first entry in the data array and print out the description for the user.
-return callback(null, data[0].description);
+    // Edge case: Handle request errors and print the error details to the screen.
+    if (error) {
+      callback("error", null);
+      return;
+    }
+  
+    // Edge Case: Breed Not Found ->  output an appropriate message if the requested breed is not found.
+    if (data.length === 0) {
+      callback("Invalid breed", null);
+      return;
+    }
 
-});
+    // Access the first entry in the data array and print out the description for the user.
+    return callback(null, data[0].description);
+
+  });
 };
 
-
+// Edge case: Handle request errors and print the error details to the screen.
 fetchBreedDescription(args[0], (error, desc) => {
-    if (error) {
-      console.log("Error fetch details: ", error);
-    } else {
-      console.log(desc);
-    }
-  });
+  if (error) {
+    console.log("Error fetch details: ", error);
+  } else {
+    console.log(desc);
+  }
+});
 
 console.log(typeof body);
 
